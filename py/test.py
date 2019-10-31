f972ce3223ef2e9867cd6bfb8507d6fc15c40ddd
Ty Wood <twood06@gmail.com>
2019-10-30 23:48:00 -0400

    day 25

--- py/test.py
+++ py/test.py
@@ -1,7 +1,18 @@
 import array as arr
 
+characters = ["Warrior", "Wizard", "Cleric"]
+
+
+
 name = input('What is your name? \n')
 print('Hello ' + name + '!')
 
-character = ["Warrior", "Wizard", "Cleric"]
+print('What would you like to play?')
 
+for character in characters:
+    print(character)
+
+input('Enter Class Choice: ')
+
+if characters == Warrior:
+    print('Welcome ' + name + 'the ' + characters)
